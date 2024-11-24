c18584c9344ec616e6cfaaa5ee9b3cb3413131e2
Erica <ekellz100@gmail.com>
2024-11-24 15:28:01 -0600

    completed lab 02_01

--- 02_more-datatypes/02_01_converter.py
+++ 02_more-datatypes/02_01_converter.py
@@ -1,6 +1,15 @@
 # Convert a string to a tuple and print out the result.
-# What do you see?
+# What do you see? 
+    #-> it separated every letter out
 # What happens if you try to iterate over your tuple of characters?
 # Do you notice any difference to iterating over the string?
+    # -> They seem to be the same
 
 string = "codingnomads"
+
+for char in string:
+    print(char)
+
+convert = tuple(string)
+#for char in convert:
+    #print(char)
