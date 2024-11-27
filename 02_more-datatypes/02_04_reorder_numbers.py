24603c0cdbc13d365ca6bd48191fb2f6dd745d0c
Erica <ekellz100@gmail.com>
2024-11-26 21:30:23 -0600

    Completed labs

--- 02_more-datatypes/02_04_reorder_numbers.py
+++ 02_more-datatypes/02_04_reorder_numbers.py
@@ -6,3 +6,19 @@
 #
 # Example input:  1,2,3,4,5,6,7,8,9,10
 # Example output: 2,4,6,8,10,9,7,5,3,1
+
+list = []
+
+while len(list) < 10:
+    try:
+        number = int(input("Please enter a number. "))
+        list.append(number)
+    except ValueError: 
+        print("Fix yo entry. ")
+print(f"The list = {list}")
+
+list_2 = [list[1], list[3], list[5], list[7], list[9]]
+print(f"List_2 = {list_2}")
+
+list_3 = [list[8], list[6], list[4], list[2], list[0]]
+print(f"List 3 = {list_3}")
