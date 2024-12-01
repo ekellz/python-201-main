6053fc3bbee33ded04ce251db198c7f1558a8608
Erica <ekellz100@gmail.com>
2024-12-01 10:02:28 -0600

    Added labs

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,14 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+file_out = open('long_words.txt', 'w')
+file_out.write('Once upon a time, there was a princess who loved her kingdom very much. Oh, and supercalifragilisticexpialidocious!')
+file_out.close()    
+
+
+file_in = open('long_words.txt', 'r')
+contents = file_in.read()
+for word in contents.split():
+    if len(word) > 20:
+        print(word)
+file_in.close()
--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,8 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+file_in = open('words.txt', 'r')
+contents = file_in.read()
+for word in contents.split():
+    if 
