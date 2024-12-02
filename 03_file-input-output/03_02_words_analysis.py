4378348ae54be5357bbea51bfb8710082c787fea
Erica <ekellz100@gmail.com>
2024-12-02 17:47:17 -0600

    labs

--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -5,7 +5,30 @@
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
 
-file_in = open('words.txt', 'r')
-contents = file_in.read()
-for word in contents.split():
-    if 
+import pathlib
+
+file_path = '/Users/ericajansen/Documents/codingnomads/python-201-main/03_file-input-output/words.txt'
+# Open the file for reading
+with open(file_path, 'r') as file_in:
+    contents = file_in.read()
+
+words = contents.split()
+sorted_words = sorted(words, key=len)
+
+shortest = sorted_words[0]
+min_length = len(sorted_words[0])
+
+for word in sorted_words:
+    if len(word) == min_length:
+        print(word)
+
+longest = sorted_words[-1]
+max_length = len(sorted_words[-1])
+
+for word in sorted_words:
+    if len(word) == max_length:
+        print(word)
+
+print(f'Total number of words: {len(words)}')
+
+
